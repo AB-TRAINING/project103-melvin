92000bf8acef6b931b884597055c940b2b3dd899
melvin <melzkaizer@gmail.com>
2021-03-15 23:35:21 +0800

    Add home page

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 103</title>
+    <link rel="stylesheet" href="calendarStyle.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col text-center">
+                <h1>Melvin's Project 103</h1>
+                <p>This is a project about making a calendar and calculator.</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">
+                            Calendar
+                        </h3>
+                        <p class="card-text">
+                            This is the calendar application.
+                        </p>
+                        <a href="/pages/calendar.php" class="btn btn-info card-link">
+                            Open Application
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">
+                            Calculator
+                        </h3>
+                        <p class="card-text">
+                            This is the calculator application.
+                        </p>
+                        <a href="/pages/calculator.php" class="btn btn-info card-link">
+                            Open Application
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
