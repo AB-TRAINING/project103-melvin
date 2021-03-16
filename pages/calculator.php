4c88284ecab20e575d341e4146239ddf469335bf
Melzkaizer <melzkaizer@gmail.com>
2021-03-16 10:09:39 +0800

    add bootstrap

---
+++ pages/calculator.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/calendar-style.css">
+    <title>Calculator</title>
+</head>
+<body>
+<div class="container mt-5">
+        <div class="row">
+            <div class="col text-center">
+                <h1>My calculator</h1>
+                <p>You can do basic calculations with this. </p>
+            </div>
+        </div>
+
+<?php 
+$result = "";
+class calculator
+{
+    var $a;
+    var $b;
+
+    function checkopration($oprator)
+    {
+        switch($oprator)
+        {
+            case '+':
+            return $this->a + $this->b;
+            break;
+
+            case '-':
+            return $this->a - $this->b;
+            break;
+
+            case '*':
+            return $this->a * $this->b;
+            break;
+
+            case '/':
+            return $this->a / $this->b;
+            break;
+
+            default:
+            return "Sorry No command found";
+        }   
+    }
+    function getresult($a, $b, $c)
+    {
+        $this->a = $a;
+        $this->b = $b;
+        return $this->checkopration($c);
+    }
+}
+
+$cal = new calculator();
+if(isset($_POST['submit']))
+{   
+    $result = $cal->getresult($_POST['n1'],$_POST['n2'],$_POST['op']);
+}
+?>
+
+<form method="post">
+<table align="center">
+    <tr>
+        <td><strong><?php echo $result; ?><strong></td>
+    </tr>
+    <tr>
+        <td>Enter 1st Number</td>
+        <td><input type="text" name="n1"></td>
+    </tr>
+
+    <tr>
+        <td>Enter 2nd Number</td>
+        <td><input type="text" name="n2"></td>
+    </tr>
+
+    <tr>
+        <td>Select Oprator</td>
+        <td><select name="op">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select></td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td><input type="submit" name="submit" value="                =                "></td>
+    </tr>
+
+</table>
+</form>
+
+<h4><a href="/index.php">Go back Home</a></h4>
+</body>
+</html>
