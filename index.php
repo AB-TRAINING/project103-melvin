3c393cbdf91a8e45b38e33534cceb1af84a3d11e
melvin <melzkaizer@gmail.com>
2021-03-17 17:42:40 +0800

    edit side bar

--- index.php
+++ index.php
@@ -4,50 +4,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project 103</title>
-    <link rel="stylesheet" href="/assets/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     
 </head>
 <body>
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col text-center">
-                <h1>Melvin's Project 103</h1>
-                <p>This is a project about making a calendar and calculator.</p>
-            </div>
+    <div id="container">
+        <div class="header">
+            <h1>Melvin's Project 103</h1>
+            <p>This is a project about making a calendar and calculator.</p>
         </div>
-        <div class="row mt-5">
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <h3 class="card-title">
-                            Calendar
-                        </h3>
-                        <p class="card-text">
-                            This is the calendar application.
-                        </p>
-                        <a href="/pages/calendar.php" class="btn btn-info card-link">
-                            Open Application
-                        </a>
+
+        <div class="content">
+            <div class="sidebar">
+                <h1>Home</h1>
+                <h4>Content</h4>
+                <li>Calendar</li>
+                <li>Calculator</li>
+            </div>
+
+        <div class="main">
+            <div class="container mt-5">
+                <div class="row mt-5">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h3>
+                                    Calendar
+                                </h3>
+                                <p class="card-text">
+                                    This is the calendar application.
+                                </p>
+                                <a href="/pages/calendar.php" class="btn btn-info card-link">
+                                    Open Application
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h3 class="card-title">
+                                    Calculator
+                                </h3>
+                                <p class="card-text">
+                                    This is the calculator application.
+                                </p>
+                                <a href="/pages/calculator.php" class="btn btn-info card-link">
+                                    Open Application
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <h3 class="card-title">
-                            Calculator
-                        </h3>
-                        <p class="card-text">
-                            This is the calculator application.
-                        </p>
-                        <a href="/pages/calculator.php" class="btn btn-info card-link">
-                            Open Application
-                        </a>
-                    </div>
-                </div>
-            </div>
         </div>
+        </div>
+    
+    <div id="footer">
+        
     </div>
+    </div>    
 </body>
 </html>
